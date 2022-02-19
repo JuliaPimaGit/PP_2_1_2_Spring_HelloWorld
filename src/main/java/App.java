f141fc308580c632ed7dd2510a3e0a1fae69e627
Julia Pimmahova <pimahovayulia@gmail.com>
2022-02-19 14:30:41 +0300

    create Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.concurrent.Callable;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean==bean2);
+        System.out.println(beanCat1==beanCat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+
+
+
+public class Cat {
+    private String catMessage;
+
+    public String getCatMessage (){
+        return catMessage;
+    }
+
+    public void setCatMessage(String catMessage) {
+        this.catMessage = catMessage;
+    }
+}
